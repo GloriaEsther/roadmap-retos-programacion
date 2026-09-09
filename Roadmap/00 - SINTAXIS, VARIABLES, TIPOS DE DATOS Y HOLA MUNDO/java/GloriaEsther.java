b2823edf33f66176ad3e16db5fb6a51322e71009
Gloria <gloriaesthermartinezmartinez@gmail.com>
2026-09-08 23:16:34 -0600

    Sintaxis en java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/GloriaEsther.java
@@ -0,0 +1,21 @@
+public class GloriaEsther {
+    
+    public static void main(String args[]){
+        // Hola soy un comentario
+        /*
+        Este es el sitio web de java:https://www.java.com/es/
+        y este tambien:https://www.oracle.com/java/
+        */
+       int num = 0;
+       String texto = "texto";
+       char c = 'x';
+       double numdouble = 10.6; 
+       final String VALOR_CONSTANTE = "valor"; 
+       boolean bool= true;
+       byte bytes = 10;
+       float numfloat = 2.4f;
+       long numlong= 200000;
+       short s=1000;
+       System.out.println("¡ Hola Java :D !");
+    }
+}
